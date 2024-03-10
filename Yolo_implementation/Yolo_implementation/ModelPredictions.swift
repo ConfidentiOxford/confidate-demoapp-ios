3f88b41f4ffa67553a10959c7f1e6f8badf93323
Galim Usaev <55399978+galimus@users.noreply.github.com>
2024-03-10 10:45:33 +0000

    Update ModelPredictions.swift

--- Yolo_implementation/Yolo_implementation/ModelPredictions.swift
+++ Yolo_implementation/Yolo_implementation/ModelPredictions.swift
@@ -5,12 +5,33 @@
 //  Created by Галим Усаев on 10.03.2024.
 //
 
-
 import Foundation
 import CoreML
 import Vision
+import UIKit
 
 struct ModelPredictions {
+    static var model: YOLOv3? = loadModel()
+    
+    static func loadModel() -> YOLOv3? {
+            do {
+                let configuration = MLModelConfiguration()
+                let modelURL = Bundle.main.url(forResource: "YOLOv3", withExtension: "mlmodelc")!
+                return try YOLOv3(contentsOf: modelURL, configuration: configuration)
+            } catch {
+                print("Ошибка загрузки модели: \(error)")
+                return nil
+            }
+        }
+    
+    static func performImageClassification(image: UIImage) -> [String] {
+            guard let pixelBuffer = processImage(image),
+                  let model = loadModel() else {
+                print("Ошибка: не удалось подготовить данные для классификации.")
+                return []
+            }
+            return predict(with: pixelBuffer, model: model)
+        }
     // Массив с названиями классов вашей модели
     static let classLabels: [String] = [
         "person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat", "traffic light",
@@ -44,26 +65,81 @@ struct ModelPredictions {
                 if !excludedClasses.contains(className) {
                                     predictedClasses.insert(className)
                                 }
-               // predictedClasses.insert(className)
-                let coordinatesIndex = boxIndex * 4
-//                let x = output.coordinates[coordinatesIndex].doubleValue
-//                let y = output.coordinates[coordinatesIndex + 1].doubleValue
-//                let width = output.coordinates[coordinatesIndex + 2].doubleValue
-//                let height = output.coordinates[coordinatesIndex + 3].doubleValue
-//
-               
                
             }
         }
- 
         return Array(predictedClasses)
     }
     
+    static func processImage(_ image: UIImage) -> CVPixelBuffer? {
+            let size = CGSize(width: 416, height: 416)
+            UIGraphicsBeginImageContextWithOptions(size, true, 2.0)
+            image.draw(in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+            let resizedImage = UIGraphicsGetImageFromCurrentImageContext()!
+            UIGraphicsEndImageContext()
+            
+            return resizedImage.toCVPixelBuffer()
+        }
     
-    // Замените это функцией, которая возвращает имя класса по индексу из вашего набора данных.
-    static func className(forIndex index: Int) -> String {
-        // Это место для вашей логики определения класса по индексу
-        // Возможно, у вас есть специальный словарь или массив, который содержит соответствие между индексами и названиями классов.
-        return "Некий класс" // Заглушка
+    static func predict(with pixelBuffer: CVPixelBuffer, model: YOLOv3) -> [String] {
+            do {
+                let input = YOLOv3Input(image: pixelBuffer)
+                let output = try model.prediction(input: input)
+                return parsePredictions(output)
+            } catch {
+                print("Ошибка предсказания: \(error)")
+                return []
+            }
+        }
+  
+}
+
+// Расширение для UIImage, чтобы помочь в преобразовании его в CVPixelBuffer
+extension UIImage {
+    func toCVPixelBuffer() -> CVPixelBuffer? {
+        let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue,
+             kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
+        var pixelBuffer: CVPixelBuffer?
+        let status = CVPixelBufferCreate(kCFAllocatorDefault,
+                                         Int(self.size.width),
+                                         Int(self.size.height),
+                                         kCVPixelFormatType_32ARGB,
+                                         attrs,
+                                         &pixelBuffer)
+        
+        guard status == kCVReturnSuccess else {
+            return nil
+        }
+        
+        CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer!)
+        
+        let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+        let context = CGContext(data: pixelData,
+                                width: Int(self.size.width),
+                                height: Int(self.size.height),
+                                bitsPerComponent: 8,
+                                bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!),
+                                space: rgbColorSpace,
+                                bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue)
+        
+        guard let drawingContext = context else {
+            return nil
+        }
+        
+        drawingContext.draw(self.cgImage!, in: CGRect(x: 0, y: 0, width: self.size.width, height: self.size.height))
+        
+        CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        
+        return pixelBuffer
+    }
+    
+    func resized(to size: CGSize) -> UIImage {
+        return UIGraphicsImageRenderer(size: size).image { _ in
+            self.draw(in: CGRect(origin: .zero, size: size))
+        }
     }
 }
+
+
+
